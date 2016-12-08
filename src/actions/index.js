d74e3bdfd8b53814f6a6a25e35690aaf4468c1bb
wumm <wumm@Ctrip.com>
2016-12-08 18:35:25 +0800

    action definition

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import {INC_CALC_VAL,DEC_CALC_VAL,ADD_COUNTER,REMOVE_COUNTER} from '../generic/actionTypes'
 
 //增加或减少计数器的值
-export const incCalcVal = function(id) {
-    return {type: INC_CALC_VAL,id:id};
-};
+export const incCalcVal = ({
+    type: INC_CALC_VAL,
+    id:id
+});
 
-export const decCalcVal = function(id) {
-    return {type: DEC_CALC_VAL,id:id};
-};
+export const decCalcVal = ({
+    type: DEC_CALC_VAL,
+    id:id
+});
 
 
 //添加或移除计数器
